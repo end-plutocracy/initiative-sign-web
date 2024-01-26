08dcb5a6430580d81a7f975b9392f21549b34cbc
Damian Birchler <damian.birchler@ost.ch>
2024-01-26 17:29:18 +0100

    Improve Java-/TypeScript.

--- app/initiatives/page.tsx
+++ app/initiatives/page.tsx
@@ -5,38 +5,37 @@ import { Anchor, Table } from '@mantine/core'
 import type { Initiative } from './initiative'
 
 export default async function Page() {
+    let data = null
     try {
-        let result = await fetch('http://localhost:8000/initiatives/');
-        let data = await result.json()
-
-        const items = data.map((initiatve: Initiative) => {
-            return (
-              <Table.Tr key={initiatve.title}>
-                <Table.Td>
-                  <Anchor href={initiatve.url}>
-                    {initiatve.title}
-                  </Anchor>
-                </Table.Td>
-              </Table.Tr>
-            );
-          });
-        
-          return (
-            <Table.ScrollContainer minWidth={800}>
-              <Table verticalSpacing="xs">
-                <Table.Thead>
-                  <Table.Tr>
-                    <Table.Th>Title</Table.Th>
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>{items}</Table.Tbody>
-              </Table>
-            </Table.ScrollContainer>
-          );
-    
-
+        const result = await fetch('http://localhost:8000/initiatives/');
+        data = await result.json()
     } catch (error) {
-        let message = `Could not fetch: ${error}`
+        const message = `Could not fetch: ${error}`
         return <p>{message}</p>
     }
+
+    const items = data.map((initiatve: Initiative) => {
+        return (
+            <Table.Tr key={initiatve.title}>
+                <Table.Td>
+                    <Anchor href={initiatve.url}>
+                        {initiatve.title}
+                    </Anchor>
+                </Table.Td>
+            </Table.Tr>
+        );
+    });
+
+    const table = <Table.ScrollContainer minWidth={800}>
+        <Table verticalSpacing="xs">
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th>Title</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{items}</Table.Tbody>
+        </Table>
+    </Table.ScrollContainer>;
+
+    return table;
 }
